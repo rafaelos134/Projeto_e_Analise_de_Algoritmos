9161dab3fa4e2c6092cdfcbbd773dfde93a81138
rafael notebook <rafa_an_jo@hotmail.com>
2025-08-24 23:49:52 -0300

    comeaçando o leetcode

--- leetcode/1-twoSum.cpp
+++ leetcode/1-twoSum.cpp
@@ -1,4 +1,34 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size();i++){
+            for (int j = 0; j < nums.size();j++){
+                if ((nums[i]+nums[j]) == target){
+                    return {i, j};}
+            }
+        }
+        return {};
+    }
+    
+};
+
 int main(){
-    int numbers[5] = {2,3,4,5}
+    Solution teste;
+
+    vector<int> meusNum = {3,5,6,2,7,4};
+    int alvo = 5;
+
+
+    vector<int> res = teste.twoSum(meusNum,alvo);
+    
+    if (!res.empty()){
+        cout << res[0];
+        cout << res[1];
+    }
+    
+
 }
