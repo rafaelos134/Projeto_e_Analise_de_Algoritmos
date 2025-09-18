51a6b79834663dae6fd40e251d6d29a52cb66c3d
notebook verlab <rafasantsh@gmail.com>
2025-09-18 19:05:39 -0300

    desafio

---
+++ leetcode/997-FindtheTownJudge.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int findJudge(int n, std::vector<std::vector<int>>& trust) {
+
+        for(int i = 0; i < trust.size();i++){
+
+            x[trust[i]] = 
+            // dicionario com quanto cada um confia em nele
+
+
+        }
+
+
+
+        return 0;
+    }
+};
+
+// dicionario com quanto cada um confia em nele
+// dicionario com quantos cada confia nos outros
+
+int main(){
+
+    Solution output;
+
+    std::vector<std::vector<int>> trust = {{1,3}, {2,3}};
+
+
+    int saida = output.findJudge(3,trust);
+
+
+}
