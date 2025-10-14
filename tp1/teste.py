0be61a530a9a398ed85174072381c0dfabf75a31
notebook verlab <rafasantsh@gmail.com>
2025-10-14 16:38:20 -0300

    modificao python

--- tp1/teste.py
+++ tp1/teste.py
@@ -2,21 +2,53 @@ def adiciona_aresta(G,u,v,flux,limit,pontucao):
     G[u] = [v,flux,limit,pontucao]
 
 
-inteiro = input()
 
-inteiro = inteiro.split(" ")
+def maxValor(G,nodeU,nodes):
 
-nTimes = int(inteiro[0])
-nCorrespondente = int(inteiro[1])
-nJogos = int(inteiro[2])
+    for x in G:
+        G[nodeU][x][0] = 2 
+    for arestas in nodes:
+        G[n]
+    
 
-G = {}
 
 
+
+
+temp = input()
+temp = temp.split(" ")
+
+nTimes = int(temp[0])
+nCorrespondente = int(temp[1])
+nJogos = int(temp[2])
+
+G = [{} for _ in range(nTimes)]
+
+print(G)
 for i in range(nTimes):
-    G[i] = []
     for j in range(nTimes):
         if (i != j):
-            G[i].append([j,0,nCorrespondente,0])
+            G[i][j] = [0,nCorrespondente,0]
+
+
+for i in range(nJogos):
+
+    temp = input()
+    temp = temp.split(" ")
+
+    nodeU = int(temp[0])
+    operador = temp[1]
+    nodeV = int(temp[2])
+
+
+    if (operador == "<"):
+        flux = 2
+    else:
+        flux = 1
+
+    G[nodeU][nodeV][0] = flux
+
+
+maxValor(G)
 
 print(G)
